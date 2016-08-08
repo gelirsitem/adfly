98ff06a8e05b0215ddcbb5a91a6daa71ec36d351
updatesvc <updatesvc@gmail.com>
2016-08-08 16:13:54 +0300

    Updated

--- link.js
+++ link.js
@@ -10,7 +10,14 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-var random_tm = getRandomInt(3000,4000);
+var random_tm1 = getRandomInt(3000,4000);
+var random_tm2 = getRandomInt(4000,5000);
+var random_tm3 = getRandomInt(6000,9000);
+var random_tm4 = getRandomInt(9000,10000);
+var random_tm5 = getRandomInt(10000,13000);
+var random_tm6 = getRandomInt(13000,16000);
+var random_tm7 = getRandomInt(16000,18000);
+var random_tm8 = getRandomInt(18000,20000);
 function clickerf(){
 	var element = document.getElementById("skip_button");
 	if ("click" in element){
@@ -25,4 +32,11 @@ function clickerf(){
 }
 
 
-setTimeout(clickerf,random_tm);
+setTimeout(clickerf,random_tm1);
+setTimeout(clickerf,random_tm2);
+setTimeout(clickerf,random_tm3);
+setTimeout(clickerf,random_tm4);
+setTimeout(clickerf,random_tm5);
+setTimeout(clickerf,random_tm6);
+setTimeout(clickerf,random_tm7);
+setTimeout(clickerf,random_tm8);
